0a72d404265be2eba27e1165ad478a51ca5e91fa
Igor <igorko.by@gmail.com>
2019-12-30 21:44:16 +0300

    Start with sect. Banner-Top

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// BOOTSTRAP
+// import 'bootstrap/dist/js/bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+// JQUERY
+import $ from 'jquery';
+global.jQuery = $;
+global.$ = $;
+
+// OWL CAROUSEL
+// import 'owl.carousel/dist/assets/owl.carousel.min.css'
+// import 'owl.carousel/dist/owl.carousel'
+
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/main.scss'
+
+// CSS
+// import './assets/css/main.css'
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// Main js file
+
+$(function () {
+
+  
+});
